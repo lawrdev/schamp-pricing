222afd27c21287bdb7dcb7a15e4c1b176624e7a9
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-02-27 04:13:33 +0100

    chore: adds AOS amd removes theme UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import {
   RecoilRoot,
-  atom,
-  selector,
-  useRecoilState,
-  useRecoilValue,
 } from 'recoil';
-import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
+import { theme } from './theme/theme'
 import AppLayout from './layout/appLayout';
 import './fonts/DMSans/dm-sans-latin-400-normal.woff2';
 import './fonts/HafferXH/HafferXH-Medium.otf'
@@ -13,28 +10,18 @@ import './fonts/HafferXH/HafferXH-Regular.otf'
 import './fonts/HafferXH/HafferXH-SemiBold.otf'
 import './fonts/SFMono/SFMonoMedium.otf'
 import './fonts/SFMono/SFMonoRegular.otf'
-
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 function App() {
+  AOS.init({
+    once: false,
+    // offset: 80,
+    easing: 'ease-in-cubic',
+    duration: 700,
+    delay: 10,
+  });
 
-  const theme = extendTheme({
-    colors: {
-      brand: {
-        main: '#197EEF'
-      },
-      backgrounds: {
-        1: '#EBEDEE',
-        2: '#0D253F',
-        3: '#EDF5FE',
-        4: '#EEF4FF',
-      }
-    },
-    fonts: {
-      body: "'DM Sans', -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif",
-      heading: "HafferXH, sans-serif",
-      mono: 'SFMonoRegular, Menlo, Monaco, Consolas,"Liberation Mono","Courier New", monospace',
-    },
-  })
   return (
     <RecoilRoot>
       <ChakraProvider theme={theme}>
