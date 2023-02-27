738def590265307d97c42bee4b7369d7e96cd56b
Lawrence <97756376+lawrdev@users.noreply.github.com>
2023-02-27 04:14:43 +0100

    style: adds navbar sticky

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,70 @@
+import { useState, useEffect } from "react";
 import { chakra, Image } from "@chakra-ui/react";
+import Cta from "./navbarComponents/cta/Cta";
 import Hamburger from "./navbarComponents/Hamburger";
+import NavList from "./navbarComponents/navList/NavList";
 
 function Navbar() {
+  const [scrollDirection, setScrollDirectione] = useState("top");
+
+  useEffect(() => {
+    let lastVal = 0;
+    window.onscroll = function () {
+      let y = window.scrollY;
+      if (y > lastVal) {
+        setScrollDirectione("down");
+      }
+      if (y < lastVal) {
+        setScrollDirectione("up");
+      }
+      if (y === 0) {
+        setScrollDirectione("top");
+      }
+      lastVal = y;
+    };
+  }, []);
+
   return (
     <>
       <chakra.nav
         display="flex"
         justifyContent="space-between"
         alignContent="center"
-        px={8}
+        px={{ base: 8, md: 24 }}
         py={6}
         bg="backgrounds.4"
+        position="sticky"
+        top={
+          scrollDirection === "top"
+            ? "0px"
+            : scrollDirection === "down"
+            ? "-100px"
+            : scrollDirection === "up"
+            ? "0px"
+            : 0
+        }
+        userSelect="none"
+        boxShadow={
+          scrollDirection === "top"
+            ? "none"
+            : scrollDirection === "down"
+            ? "0px 0px 5px #0D253F50"
+            : scrollDirection === "up"
+            ? "0px 0px 5px #0D253F50"
+            : "none"
+        }
+        transition="all 0.75s cubic-bezier(0.645,0.045,0.355,1)"
+        zIndex="2"
       >
         <Image
           src="https://res.cloudinary.com/sendchamp/image/upload/v1647472661/Sendchamp%20Website/logos/NavLogo_nhyqah.svg"
           alt="sendchamp"
         />
+        <NavList />
 
         <Hamburger />
+
+        <Cta />
       </chakra.nav>
     </>
   );
